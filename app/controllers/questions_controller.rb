14e9b89b2cdcb945a56d846ba6b0e3b0380ea098
Alex Smirnov <sas2job@gmail.com>
2023-06-19 09:11:46 +0300

    Add flash message

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,6 +14,7 @@ class QuestionsController < ApplicationController
   def update
     @question = Question.find_by id: params[:id]
     if @question.update(questions_params)
+      flash[:success] = "Question updated!"
       redirect_to questions_path
     else
       render :edit
@@ -27,12 +28,14 @@ class QuestionsController < ApplicationController
   def destroy
     @question = Question.find_by id: params[:id]
     @question.destroy
+    flash[:success] = "Question deleted!"
     redirect_to questions_path
   end
 
   def create
     @question = Question.new questions_params
     if @question.save
+      flash[:success] = "Question created!"
       redirect_to questions_path
     else
       render :new
