44bd40203689478c31aae4a8543b77017c0ae475
Alex Smirnov <sas2job@gmail.com>
2023-06-19 09:53:37 +0300

    Add concern with error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
-  rescue_from ActiveRecord::RecordNotFound, with: :notfound
-
-  private
-
-  def notfound(exception)
-    logger.warn exception
-    render file: 'public/404.html', status: :not_found, layout: false
-  end
+  include ErrorHandling
 end
